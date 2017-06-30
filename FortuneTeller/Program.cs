9549beab44de01d1e9515ca0f2a9da1f92e5638b
Nick Wise <nickcwise@gmail.com>
2017-06-30 16:28:29 -0400

    Progress on FortuneTeller project

--- FortuneTeller/Program.cs
+++ FortuneTeller/Program.cs
@@ -32,84 +32,108 @@ namespace FortuneTeller
             {
                 Console.Write("\nRed \nOrange \nYellow \nGreen \nBlue \nIndigo \nViolet\n\nEnter Color: \a");
             }
-                string userColorHelp = Console.ReadLine(); //userColorHelp comes after user types Help for colors displayed
+                string userColorHelp = Console.ReadLine(); //userColorHelp comes after user types Help for ROYGBIV display
 
             Console.Write("Please enter the number of siblings you have: ");
             int userSibling = int.Parse(Console.ReadLine());
 
-            Console.Write("Thank you for your submission! \nPress enter to see your future!");
+            Console.Write("Thank you for your submission! \nPress enter to see your future!\n\n");
             Console.ReadKey();
+
+            Console.Write(firstName+ " ");
+            Console.Write(lastName);
 
             //Start conditionals for variables
 
             //Conditional for user age
             if (userAge % 2 != 0)
             {
-                Console.Write("You will retire in 20 years \n");
+                Console.Write(" will retire in 20 years");
             }
             else if (userAge % 2 == 0)
             {
-                Console.Write("You will retire in 30 years \n");
+                Console.Write(" will retire in 30 years");
             }
 
             //Conditional for user birth month
             if (userBirthMonth >= 1 && userBirthMonth <= 4)
             {
-                Console.Write("You will have $20,000 in the bank\n");
+                Console.Write(" with $20,000 in the bank,");
             }
             else if (userBirthMonth >= 5 && userBirthMonth <= 8)
             {
-                Console.Write("You will have $50,000 in the bank\n");
+                Console.Write(" with $50,000 in the bank,");
             }
             else if (userBirthMonth >= 9 && userBirthMonth <= 12)
             {
-                Console.Write("You will have $75,000 in the bank\n");
+                Console.Write(" with $75,000 in the bank,");
             }
             else
             {
-                Console.Write("You will have $0 in the bank\n");
+                Console.Write(" with $0 in the bank,");
+            }
+
+            //Conditional for number of siblings
+
+            if (userSibling == 0)
+            {
+                Console.Write(" a vacation home on a mountain in Colorado,");
+            }
+            else if (userSibling == 1)
+            {
+                Console.Write(" a vacation home in the Lofoten Islands off the coast of Norway,");
+            }
+            else if (userSibling == 2)
+            {
+                Console.Write(" a vacation home in New York City,");
+            }
+            else if (userSibling == 3)
+            {
+                Console.Write(" a vactaion home in Iceland,");
+            }
+            else
+            {
+                Console.Write(" a vacation home in New Jersey,");
             }
 
             //Conditional for favorite color
 
             if (userColor == "Red")
             {
-                Console.Write("You will drive a 1997 Toyota 4Runner\n");
+                Console.Write(" and will drive a 1997 Toyota 4Runner.\n");
             }
             else if (userColor == "Orange")
             {
-                Console.Write("You will drive an exact replica of the Batmobile\n");
+                Console.Write(" and will drive an exact replica of the Batmobile.\n");
             }
             else if (userColor == "Yellow")
             {
-                Console.Write("You will drive a Maserati\n");
+                Console.Write(" and will drive a Maserati.\n");
             }
             else if (userColor == "Green")
             {
-                Console.Write("You will drive a neon gree Lincoln Town Car\n");
+                Console.Write(" and will drive a neon green Lincoln Town Car.\n");
             }
             else if (userColor == "Blue")
             {
-                Console.Write("You will forever live on a sailboat with no cabin\n");
+                Console.Write(" and will forever live on a sailboat with no cabin.\n");
             }
             else if (userColor == "Indigo")
             {
-                Console.Write("You will fly John Travolta's airplane to and from his house\n");
+                Console.Write(" and will fly John Travolta's airplane to and from his house.\n");
             }
             else if (userColor == "Violet")
             {
-                Console.Write("You will hang around public parks and try and ride people's kites\n");
+                Console.Write(" and will hang around public parks and try and ride people's kites.\n");
             }
             else
             {
-                Console.Write("You will have no means of travel\n");
+                Console.Write(" and will have no means of travel.\n");
             }
 
-            //Conditional for number of siblings
 
 
-               
-
+            //[First Name] [Last Name] will retire in [# of Years] with [Amount of Money] in the bank, a vacation home in [Location] and a [Mode of Transportation].
 
         }
     }
